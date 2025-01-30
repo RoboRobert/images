b12aeb40033d8c71ebbc97e938c9420c7a88a218
RoboRobert <nathan.estep@gmail.com>
2025-01-29 23:37:24 -0600

    bad popularity algorithm

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const ctx = canvas.getContext("2d");
 const editedCanvas = document.getElementById("editedCanvas");
 const editedctx = editedCanvas.getContext("2d");
 
-var operation_type = document.getElementById("operation-type");
 
-draw_image("test.png");
+var operation_type = document.getElementById("operation-type");
+var num_bits = document.getElementById("num-bits");
+
+// draw_image("test.png");
 
 const ops = {
     UNIFORM_QUANT : 1,
@@ -26,23 +28,24 @@ document.getElementById('image-selector').addEventListener('change', function(ev
             draw_image(e.target.result);
         };
 
-        reader.readAsDataURL(file); // Read the file as a data URL
+        reader.readAsDataURL(file);
     }
 });
 
-// Function that draws an image to the first canvas
+// Draws an image to the first canvas
 function draw_image(source) {
     const img = new Image();
     img.onload = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear any previous image
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height); // Draw the image on the canvas
     };
-    img.src = source; // Set the image source to the loaded file
+    img.src = source; // Set the image source
 }
 
 // Detects when the image modification button has been pressed and acts on it
 document.getElementById("modifier").addEventListener('click', function(event) {
     let operation = operation_type.value;
+    let number_bits = Number(num_bits.value);
 
     var imageData = ctx.getImageData(0,0, canvas.width, canvas.height);
 
@@ -50,10 +53,10 @@ document.getElementById("modifier").addEventListener('click', function(event) {
 
     switch (Number(operation)) {
         // Uniform quantization
-        case ops.UNIFORM_QUANT : data = quant_1(data, 16); break;
+        case ops.UNIFORM_QUANT : data = uniform_quant(data, number_bits); break;
 
         // Population-based quantization
-        case ops.POP_QUANT : console.log("QUANT 2"); break;
+        case ops.POP_QUANT :data = popularity_quant(data, number_bits); break;
 
         // median cut quantization
         case ops.MEDIAN_QUANT : console.log("QUANT 3"); break;
@@ -87,10 +90,14 @@ function map(input, start_bits, end_bits) {
 // Uniform quantizer.
 // Data is an array of pixels, target is the number of bits of color depth to target.
 // Assumes the incoming image has 24-bit color depth
-function quant_1(data, target) {
-    let red_bits = Math.ceil(target/3);
+function uniform_quant(data, target) {
     let green_bits =  Math.ceil(target/3);
-    let blue_bits =  Math.floor(target/3);
+    let red_bits = Math.ceil((target-green_bits)/2);
+    let blue_bits =  target-red_bits-green_bits;
+
+    // let red_bits = Math.ceil(target/3);
+    // let green_bits = Math.ceil((target-red_bits)/2);
+    // let blue_bits =  target-red_bits-green_bits;
 
     console.log(red_bits,green_bits,blue_bits);
 
@@ -102,5 +109,76 @@ function quant_1(data, target) {
         data[i] = map(red, 8, red_bits);
         data[i+1] = map(green, 8, green_bits);
         data[i+2] = map(blue, 8, blue_bits);
+    }
+}
+
+class Popularity {
+    value;
+    occurrences;
+
+    constructor(value, occurrences) {
+        this.value = value;
+        this.occurrences = occurrences;
+    }
+}
+
+// Maps an input to its closest neighbor out of n closest occurrences
+function pop_map(input, n, occurrences) {
+    let dist = 10000;
+    let nearest = input;
+    for(var i = 0; i < n; i++) {
+        distance = Math.abs(occurrences[i].value-input)
+        if(distance < dist) {
+            nearest = occurrences[i].value;
+            dist = distance;
+        }
+    }
+    
+    return nearest;
+}
+
+// Popularity-based quantizer.
+// Data is an array of pixels, target is the number of bits of color depth to target.
+// Assumes the incoming image has 24-bit color depth
+function popularity_quant(data, target) {
+    let green_bits =  Math.ceil(target/3);
+    let red_bits = Math.ceil((target-green_bits)/2);
+    let blue_bits =  target-red_bits-green_bits;
+
+    let red_map = new Array()
+    let green_map = new Array()
+    let blue_map = new Array()
+
+    for(var i = 0; i < 256; i++) {
+        red_map.push(new Popularity(i,0));
+        green_map.push(new Popularity(i,0));
+        blue_map.push(new Popularity(i,0));
+    }
+
+    // Counts common occurrences
+    for(var i = 0; i < data.length; i += 4) {
+        red_map[data[i]].occurrences += 1;
+        green_map[data[i+1]].occurrences += 1;
+        blue_map[data[i+2]].occurrences += 1;
+    }
+
+    red_map.sort((a,b) => a.occurrences < b.occurrences);
+    blue_map.sort((a,b) => a.occurrences < b.occurrences);
+    green_map.sort((a,b) => a.occurrences < b.occurrences);
+
+    console.log(red_map);
+    console.log(green_map);
+    console.log(blue_map);
+
+    for(var i = 0; i < data.length; i += 4) {
+        let red = data[i];
+        let green = data[i+1];
+        let blue = data[i+2];
+
+        data[i] = pop_map(red, Math.pow(2, red_bits), red_map);
+        data[i+1] = pop_map(green, Math.pow(2, green_bits), green_map);
+        data[i+2] = pop_map(blue, Math.pow(2, blue_bits), blue_map);
+
+        // console.log(data[i], data[i+1], data[i+2]);
     }
 }
